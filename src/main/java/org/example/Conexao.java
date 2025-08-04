19738d456c59223713478106d6b7c361c8896168
mozartl2 <lucas.mozsart@gmail.com>
2025-08-04 08:40:53 -0300

    Novas funções
    Adicionado novas funções

--- src/main/java/org/example/Conexao.java
+++ src/main/java/org/example/Conexao.java
@@ -1,23 +1,58 @@
 package org.example;
-
 import com.mysql.cj.protocol.Resultset;
-
 import java.sql.*;
 
 public class Conexao {
-    private static String url = "jdbc:mysql://192.168.18.10:3306/dividas";
-    private static String user = "pc-payer";
-    private static String senha = "Lucas@101";
+    private static final String url = "jdbc:mysql://192.168.18.10:3306/dividas";
+    private static final String user = "pc-payer";
+    private static final String senha = "Lucas@101";
     private static Connection conexao = null;
 
     public static void conectar() throws SQLException {
         conexao = DriverManager.getConnection(url, user, senha);
     }
 
-    public static ResultSet buscarClDevedores() throws SQLException {
-        PreparedStatement stm = conexao.prepareStatement("SELECT * FROM view_clientes_devedores");
-        ResultSet rs = stm.executeQuery();
-        return rs;
+    //Busca todos os clientes
+    public static ResultSet buscarTodosCl() throws SQLException {
+        PreparedStatement stm = conexao.prepareStatement("SELECT * FROM clientes");
+        return stm.executeQuery();
     }
 
+    //Busca os clientes que tem alguma parcela com o status diferente de 'paga'
+    public static ResultSet buscarClDevedores() throws SQLException {
+        PreparedStatement stm = conexao.prepareStatement("SELECT * FROM view_clientes_devedores");
+        return stm.executeQuery();
+    }
+
+    //Insere um novo cliente sem telefone
+    public static int insereNovoCliente(String nome, String sobrenome, String loja) throws SQLException {
+        PreparedStatement stm = conexao.prepareStatement("INSERT INTO clientes (nome, sobrenome, loja) VALUES (?, ?, ?)");
+        stm.setString(1, nome);
+        stm.setString(2, sobrenome);
+        stm.setString(3, loja);
+        return stm.executeUpdate();
+    }
+    //Insere um novo cliente com telefone
+    public static int insereNovoCliente(String nome, String sobrenome, String loja, String telefone) throws SQLException {
+        PreparedStatement stm = conexao.prepareStatement("INSERT INTO clientes (nome, sobrenome, loja, telefone) VALUES (?, ?, ?, ?)");
+        stm.setString(1, nome);
+        stm.setString(2, sobrenome);
+        stm.setString(3, loja);
+        stm.setString(4, telefone);
+        return stm.executeUpdate();
+    }
+
+    //Apaga um cliente
+
+    //Insere um nova divida
+    public static int insereDivida(int idCliente, String dataContratacao, float valor, int qtdParcelas) throws SQLException {
+        PreparedStatement stm = conexao.prepareStatement("CALL insere_dividas(?, ?, ?, ?);");
+        stm.setInt(1, idCliente);
+        stm.setString(2, dataContratacao);
+        stm.setFloat(3, valor);
+        stm.setInt(4,qtdParcelas);
+        return stm.executeUpdate();
+    }
+
+    //Da a baixa em uma parcela
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,68 @@
 package org.example;
 import java.sql.*;
 import java.sql.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.print("Hello and welcome!");
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual ação quer realizar?" +
+                "\n1.Ver cleintes devedores" +
+                "\n2.Ver todos os clientes" +
+                "\n3.Incluir um cliente" +
+                "\n4.Insere uma nova dívida"
+        );
+        int userInput = scanner.nextInt();
+        scanner.nextLine();
         Conexao.conectar();
-        ResultSet rs = Conexao.buscarClDevedores();
-        while(rs.next()){
-            System.out.println("Nome: " + rs.getString("nome") + " Loja: " + rs.getString("loja"));
+
+        ResultSet rs = null;
+        switch(userInput){
+            case 1:
+                rs = Conexao.buscarClDevedores();
+                while(rs.next()){
+                    System.out.println("Nome: " + rs.getString("nome")
+                    + "\n, Sobrenome: " + rs.getString("sobrenome")
+                    + "\n, Loja: " + rs.getString("loja")
+                    + "\n, Telefone: " + rs.getString("telefone"));
+                }
+            break;
+            case 2:
+                rs = Conexao.buscarTodosCl();
+                while(rs.next()){
+                    System.out.println("Nome: " + rs.getString("nome")
+                    + ", Sobrenome: " + rs.getString("sobrenome")
+                    + ", Loja: " + rs.getString("loja")
+                    + ", Telefone: " + rs.getString("telefone"));
+                }
+            break;
+            case 3:
+                System.out.println("Digite o nome do cliente: ");
+                String nomeCliente = scanner.nextLine();
+                System.out.println("Digite o sobrenome do cliente:");
+                String sobrenomeCliente = scanner.nextLine();
+                System.out.println("Digite o nome da Loja: ");
+                String lojaCliente = scanner.nextLine();
+                System.out.println("Digite o telefone: ");
+                String telefoneCliente = scanner.nextLine();
+                int retornoQueryInsert = Conexao.insereNovoCliente(nomeCliente, sobrenomeCliente, lojaCliente, telefoneCliente);
+                System.out.println("Retorno da chamada: " + retornoQueryInsert);
+            break;
+            case 4:
+                System.out.println("Id do cliente: ");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+                System.out.println("Qual da data?");
+                String data = scanner.nextLine();
+                System.out.println("Valor:");
+                float valor = scanner.nextFloat();
+                System.out.println("Em quantas parcelas?");
+                int parcelas = scanner.nextInt();
+                int retornoInsert = Conexao.insereDivida(id, data, valor, parcelas);
+                System.out.println("Retorno do Insert: " + retornoInsert);
+            break;
         }
     }
 }
